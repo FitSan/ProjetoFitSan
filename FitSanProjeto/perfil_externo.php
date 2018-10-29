51be3717fd0b28aba9fd1989d3bf79e4f88f595d
karen <karen@macbook>
2018-10-29 01:31:49 -0300

    Correções necessarias nas abas no perfil e mensagem no perfil do usuário sem vinculo quando acessado por outro usuario.

--- FitSanProjeto/perfil_externo.php
+++ FitSanProjeto/perfil_externo.php
@@ -19,9 +19,9 @@ if ($linha = mysqli_fetch_array($resultado)) {
             <h1>
                 Perfil de Usuário
             </h1>
-            <ol class="breadcrumb">
+<!--            <ol class="breadcrumb">
                 <li> <?= breadcrumbs() ?></li>
-            </ol>
+            </ol>-->
         </section>
 
 
@@ -169,6 +169,37 @@ if ($linha = mysqli_fetch_array($resultado)) {
 
                     <?php
                     if (tipoLogado("profissional") && ($linha['tipo'] == 'aluno')) {
+                        $vinculado = dbquery("select count(*) as total from usuario u left join vinculo v on v.aluno_id = u.id where v.profissional_id = ".$_SESSION['id']." and v.aluno_id = ".$linha['id']." and u.status = 'ativado' and u.tipo_id=1");
+                        $vinculado = ($vinculado ? (intval($vinculado[0]['total']) > 0) : false);
+                    } else {
+                        $vinculado = null;
+                    }
+                    if ($vinculado === false){
+                        ?>
+                
+
+                <div class="col-md-9">
+                            <div class="box box-primary">
+                                <div class="box-body box-profile">
+                                    <div class="alert alert-success" role="alert">
+                                        <h4 class="alert-heading"><?php
+                                                    if (estaLogado()) {
+                                                        echo 'Olá, ' . exibirName();
+                                                    }
+                                                    ?></h4>
+                                                <p> Este aluno não esta vinculado a você!</p>
+                                                <hr>
+                                                <p class="mb-0">Para ter acesso aos dados do aluno entre em contato ou solicite seguir o aluno.</p>
+                                            </div>
+                                        </div>
+                            </div>
+                        </div>
+
+                
+              
+                <?php
+                    }
+                    if ($vinculado){
                         $aba = (!empty($_GET['aba']) ? $_GET['aba'] : 'timeline');
                         ?>
 
@@ -185,17 +216,160 @@ if ($linha = mysqli_fetch_array($resultado)) {
                                 <div class="tab-content">
                                     <div class="tab-pane <?php if ($aba == 'timeline') echo ' active'; ?>" id="timeline">
                                         <!-- Post -->
+                                        
                                         Linha do tempo
+                                        
                                         <!-- /.post -->
                                     </div>
 
 
                                     <div class="tab-pane <?php if ($aba == 'atividadesExtras') echo ' active'; ?>" id="atividadesExtras">
                                         <!-- Post -->
-                                        Atividade Extra
+                                <?php
+                                
+
+//referente ao formulário
+if (!empty($id)) {
+    $query_alterar = "select * from ativ_extras where aluno_id = " . mysqliEscaparTexto($_GET['id']) . " and id= " . mysqliEscaparTexto($id);
+    $resultado_alterar = mysqli_query($conexao, $query_alterar) or die_mysql($query_alterar, __FILE__, __LINE__);
+    $linha_alterar = ($resultado_alterar?mysqli_fetch_array($resultado_alterar):array());
+    $query_exe_alterar = "select * from ativ_extras_exercicios where ativ_extras_id= " . mysqliEscaparTexto($id);
+    $resultado_exe_alterar = mysqli_query($conexao, $query_exe_alterar) or die_mysql($query_exe_alterar, __FILE__, __LINE__);
+    $linha_alterar['exercicios'] = array();
+    while ($linha2 = mysqli_fetch_array($resultado_exe_alterar)) $linha_alterar['exercicios'][] = $linha2['exercicio'];
+} else {
+    $linha_alterar = array();
+}
+
+//referente à paginação
+$query_pagina = "
+select
+    count(ativ_extras.id) as total
+from
+    ativ_extras join
+    usuario on usuario.id=ativ_extras.aluno_id
+where
+    usuario.id= " . mysqliEscaparTexto($_GET['id']) . " and
+    ativ_extras.visualizacao = 'PUBLICO'
+";
+$resultado_pagina = mysqli_query($conexao, $query_pagina) or die_mysql($query_pagina, __FILE__, __LINE__);
+$paginacao = ($resultado_pagina?mysqli_fetch_array($resultado_pagina):array());
+$paginacao = array_merge(array(
+    'total' => 0,
+    'quantidade' => (!empty($_GET['quantidade']) ? $_GET['quantidade'] : 10),
+    'pagina' => (!empty($_GET['pagina']) ? $_GET['pagina'] : 1),
+), array_map('intval', (array)$paginacao));
+$paginacao['offset'] = (($paginacao['pagina'] - 1) * $paginacao['quantidade']);
+$paginacao['paginas'] = ceil($paginacao['total'] / $paginacao['quantidade']);
+
+if ($paginacao['total'] > 0){
+?>
+<ul class="timeline timeline-inverse"> 
+<?php
+//referente à consulta
+$query = "
+select
+    ativ_extras.*,
+    usuario.nome,
+    usuario.sobrenome,
+    usuario.foto
+from 
+    ativ_extras join
+    usuario on usuario.id=ativ_extras.aluno_id
+where
+    usuario.id= " . mysqliEscaparTexto($_GET['id']) . " and
+    ativ_extras.visualizacao = 'PUBLICO'
+order by
+    ativ_extras.datahora desc
+limit
+    " . $paginacao['quantidade'] . "
+offset
+    " . $paginacao['offset']
+;
+$resultado = mysqli_query($conexao, $query) or die_mysql($query, __FILE__, __LINE__);
 
 
-                                        <!-- /.post -->
+
+$dataanterior = '';
+while ($linha = mysqli_fetch_array($resultado)) {
+    $dataatual = date('d/m/Y', dataParse($linha['datahora']));
+    if ($dataanterior != $dataatual){
+?>
+                    <li class="time-label">
+                        <span class="bg-red">
+                            <?= $dataatual ?>
+                        </span>
+                    </li>
+                    
+<?php
+        $dataanterior = $dataatual;
+    }
+?>
+                    
+                    <li>
+                        <i class="fa fa-user bg-aqua"></i>
+
+                        <div class="timeline-item bg-danger">
+                            <span class="time"><i class="fa fa-clock-o"></i> <?= date('H:i:s', dataParse($linha['datahora'])) ?></span>
+
+                            <h3 class="timeline-header"><a href="#"><?= htmlentities($linha['titulo']) ?></a>
+<?php
+    $query2 = "
+select
+    *
+from
+    ativ_extras_exercicios
+where 
+    ativ_extras_id= " . mysqliEscaparTexto($linha['id'])
+;
+    if ($resultado2 = mysqli_query($conexao, $query2)){
+        while ($linha2 = mysqli_fetch_array($resultado2)){
+?>
+                                <span class="label label-info"><?= htmlspecialchars($linha2['exercicio']) ?></span>
+<?php
+        }
+        mysqli_free_result($resultado2);
+    }
+?>
+                            </h3>
+
+                            <div class="timeline-body">
+                                <?= nl2br(htmlentities($linha['texto'])) ?>
+                            </div>
+
+                            
+                        </div>
+                    </li>
+                    
+<?php
+}
+if ($dataanterior){
+?>
+                    
+                    <li>
+                        <i class="fa fa-clock-o bg-gray"></i>
+                    </li>
+<?php } ?>
+                </ul> 
+                    
+<?php if ($paginacao['paginas'] > 1){ ?>
+        <div class="box-footer clearfix">
+            <ul class="pagination pagination-sm no-margin">
+                <li class="<?php echo (($paginacao['pagina'] == 1) ? 'disabled' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=atividadesExtras&id=<?php echo $_GET['id'] ?>">&laquo;</a></li>
+<?php for ($pag = 1; $pag <= $paginacao['paginas']; $pag++){ ?>
+                <li class="<?php echo (($paginacao['pagina'] == $pag) ? 'active' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=atividadesExtras&id=<?php echo $_GET['id'] ?>&pagina=<?php echo $pag ?>"><?php echo $pag ?></a></li>
+<?php } ?>
+                <li class="<?php echo (($paginacao['pagina'] == $paginacao['paginas']) ? 'disabled' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=atividadesExtras&id=<?php echo $_GET['id'] ?>&pagina=<?php echo $paginacao['paginas'] ?>">&raquo;</a></li>
+            </ul>
+        </div>
+    <?php } ?>  
+<?php } else { ?>
+<div class="text-center"><h3><b>Não foi realizado nenhuma atividade extra ainda.</b></h3></div>
+<?php } ?>
+              
+              
+                                
+                                <!-- /.post -->
                                     </div>
                                     
                                     <div class="tab-pane<?php if ($aba == 'pesosMedidas') echo ' active'; ?>" id="pesosMedidas">
@@ -212,10 +386,177 @@ if ($linha = mysqli_fetch_array($resultado)) {
                                     <div class="tab-pane<?php if ($aba == 'treinosPlanilha') echo ' active'; ?>" id="treinosPlanilha">
                                         
                                         <!-- Post -->
-                                        
-                                        Treinos Realizados 
-                                        
-                                        <!-- /.post -->
+                                
+                                <?php
+$query = array();
+$query['select'] = array(
+    'a.planilha_id',
+    'p.profissional_id',
+    'p.musculo_cardio_id',
+    'p.exercicio_id',
+    'p.grupo',
+    'p.series',
+    'p.repeticoes',
+    'p.carga',
+    'p.intervalo',
+    'p.tempo',
+    'g.nome grupomusc',
+    'e.nome exercicio',
+    'e.descricao exercicio_desc',
+    'e.foto exercicio_foto',
+    'f.datahora',
+    'z.planilha_feito_id',
+    'u.nome as profissional_nome',
+    'u.sobrenome as profissional_sobrenome',
+    'u.email as profissional_email',
+);
+$query['from'] = "
+    planilha_aluno a join
+    planilha_aluno_feito f on f.planilha_aluno_id = a.id join
+    planilha_aluno_exercicio z on z.planilha_feito_id = f.id join
+    planilha_tabela p on p.planilha_id = a.planilha_id and p.id = z.planilha_tabela_id join
+    planilha_grupoMuscuCardio g on g.id = p.musculo_cardio_id join
+    planilha_exercicio e on e.id = z.exercicio join
+    usuario u on u.id = p.profissional_id
+";
+$query['where'] = array(
+    "a.aluno_id = " . mysqliEscaparTexto($_GET['id']),
+);
+
+//referente à paginação
+$query_pagina = $query;
+$query_pagina['select'] = "count(*) as total";
+$resultado_pagina = dbquery($query_pagina);
+$paginacao2 = ($resultado_pagina?$resultado_pagina[0]:array());
+$paginacao2 = array_merge(array(
+    'total' => 0,
+    'quantidade' => (!empty($_GET['quantidade']) ? $_GET['quantidade'] : 10),
+    'pagina' => (!empty($_GET['pagina2']) ? $_GET['pagina2'] : 1),
+), array_map('intval', (array)$paginacao2));
+$paginacao2['offset'] = (($paginacao2['pagina'] - 1) * $paginacao2['quantidade']);
+$paginacao2['paginas'] = ceil($paginacao2['total'] / $paginacao2['quantidade']);
+
+
+$query['order'] = "f.datahora desc,p.grupo,exercicio";
+$query['outro'] = "limit " . $paginacao2['quantidade'] . " offset " . $paginacao2['offset'];
+
+
+
+$resultado = dbquery($query);
+?>
+
+<section class="content-header">   
+<h3 class="box-title">Exercícios Feitos</h3>
+</section><br>
+<?php if (!empty($resultado)){ ?>
+<div class="tab-pane" id="timeline">
+    <ul class="timeline timeline-inverse">
+        <?php
+$dataanterior = $grupo_atual = $prof_atual = ''; $anterior = null;
+foreach ($resultado as $linha) {
+    $dataatual = date('d/m/Y', dataParse($linha['datahora']));
+    if ($grupo_atual && (
+        ($dataanterior != $dataatual) ||
+        ($prof_atual != $linha['profissional_id']) ||
+        ($grupo_atual != $linha['grupo'])
+    )){
+?>
+                    </table>
+                    
+                   
+                    
+                </div>
+
+            </div>
+        </li> 
+<?php
+    }
+    if ($dataanterior != $dataatual){
+?>
+
+        <li class="time-label">
+            <span class="bg-red">
+                <?= $dataatual ?>
+            </span>
+        </li>
+            
+<?php
+        $dataanterior = $dataatual;
+        $prof_atual = $grupo_atual = '';
+    }
+    if (($prof_atual != $linha['profissional_id']) || ($grupo_atual != $linha['grupo'])){
+        $grupo_atual = $linha['grupo'];
+        $prof_atual = $linha['profissional_id'];
+?>            
+        <li>
+            <i class="fa fa-thumbs-o-up bg-blue"></i>
+
+            <div class="timeline-item">
+                <span class="time"><i class="fa fa-clock-o"></i> <?= date('H:i:s', dataParse($linha['datahora'])) ?></span>
+
+                <h3 class="timeline-header"><strong><?php echo htmlspecialchars($linha['grupo']); ?></strong> - por <?php echo htmlspecialchars($linha['profissional_nome'] . ' ' . $linha['profissional_sobrenome']); ?></h3>
+                
+
+                <div class="timeline-body">
+                    
+                    <table class="table table-striped planilha dataTable">
+                        <tr>
+                            <th>Exercício</th>
+                            <th>Séries</th>
+                            <th>Repetições</th>                      
+                            <th>Carga(Kg)</th>
+                            <th>Intervalo</th>
+                            <th>Tempo</th>
+                        </tr>
+<?php
+    }
+?>
+                        <tr>
+                            <td><?php echo htmlentities($linha['exercicio']) ?><b class="label label-danger"><?php echo htmlentities($linha['grupomusc']) ?></b></td>
+                            <td><?php echo htmlentities($linha['series']) ?></td>
+                            <td><?php echo htmlentities($linha['repeticoes']) ?></td>
+                            <td><?php echo htmlentities($linha['carga']) ?></td>
+                            <td><?php echo htmlentities($linha['intervalo']) ?></td>
+                            <td><?php echo htmlentities($linha['tempo']) ?></td>
+                        </tr>
+<?php
+    $anterior = $linha;
+}
+if ($grupo_atual){
+?>
+                    </table>
+                    
+                   
+                    
+                </div>
+
+            </div>
+        </li> 
+        <li>
+            <i class="fa fa-clock-o bg-gray"></i>
+        </li>
+        <?php
+}
+        ?>
+    
+    </ul>
+<?php if ($paginacao2['paginas'] > 1){ ?>
+        <div class="box-footer clearfix">
+            <ul class="pagination pagination-sm no-margin">
+                <li class="<?php echo (($paginacao2['pagina'] == 1) ? 'disabled' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=treinosPlanilha&id=<?php echo $_GET['id'] ?>">&laquo;</a></li>
+<?php for ($pag = 1; $pag <= $paginacao2['paginas']; $pag++){ ?>
+                <li class="<?php echo (($paginacao2['pagina'] == $pag) ? 'active' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=treinosPlanilha&id=<?php echo $_GET['id'] ?>&pagina2=<?php echo $pag ?>"><?php echo $pag ?></a></li>
+<?php } ?>
+                <li class="<?php echo (($paginacao2['pagina'] == $paginacao2['paginas']) ? 'disabled' : '') ?>"><a href="<?php echo basename(__FILE__) ?>?aba=treinosPlanilha&id=<?php echo $_GET['id'] ?>&pagina2=<?php echo $paginacao2['paginas'] ?>">&raquo;</a></li>
+            </ul>
+        </div>
+<?php } ?>
+</div>
+<?php } else { ?>
+<div class="text-center"><h3><b>Não foi realizado nenhum exercício ainda.</b></h3></div>
+    <?php } ?> 
+                                
+                                <!-- /.post -->
 
                                     </div>
                                     <div class="tab-pane <?php if ($aba == 'avaliacoes') echo ' active'; ?>" id="avaliacoes">
@@ -234,6 +575,32 @@ if ($linha = mysqli_fetch_array($resultado)) {
                     }
 
                     if (tipoLogado("aluno") && ($linha['tipo'] == 'profissional')) {
+                        $vinculado = dbquery("select count(*) as total from usuario u left join vinculo v on v.profissional_id = u.id where v.aluno_id = ".$_SESSION['id']." and v.profissional_id = ".$linha['id']." and u.status = 'ativado' and u.tipo_id=1");
+                        $vinculado = ($vinculado ? (intval($vinculado[0]['total']) > 0) : false);
+                    } else {
+                        $vinculado = null;
+                    }
+                    if ($vinculado === false){
+                        ?>
+        <div class="col-md-9">
+            <div class="box box-primary">
+                <div class="box-body box-profile">
+                    <div class="alert alert-success" role="alert">
+                        <h4 class="alert-heading"><?php
+                            if (estaLogado()) {
+                                echo 'Olá, ' . exibirName();
+                            }
+                            ?></h4>
+                        <p> Este Profissional não esta vinculado a você!</p>
+                        <hr>
+                        <p class="mb-0">Para ter acesso aos dados entre em contato ou solicite seguir o profissional.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+                    }
+                    if ($vinculado){
                         $aba = (!empty($_GET['aba']) ? $_GET['aba'] : 'timeline');
                ?>
                         <div class="col-md-9">
@@ -317,38 +684,7 @@ $resultado = mysqli_query($conexao, $query);
                
     
                 
-<?php }    
-           
-           if (tipoLogado("profissional") && ($linha['tipo'] == 'profissional')) {
-               ?>
-                <div class="col-md-9">
-                            <div class="nav-tabs-custom">
-                                <ul class="nav nav-tabs">
-                                    <li <?php if ($aba == 'timeline') echo ' class="active"'; ?>><a href="#timeline" data-toggle="tab">Linha do tempo</a></li>                    
-                                    <li <?php if ($aba == 'dicas') echo ' class="active"'; ?>><a href="#dicas" data-toggle="tab">Dicas</a></li>
-
-                                </ul>
-                                <div class="tab-content">
-                                    <div class="tab-pane <?php if ($aba == 'timeline') echo ' active'; ?>" id="timeline">
-                                        <!-- Post -->
-                                        Discutir o que colocar aqui!
-                                        <!-- /.post -->
-                                    </div>
-                                    <div class="tab-pane <?php if ($aba == 'dicas') echo ' active'; ?>" id="dicas">
-
-                                        <!-- Post -->
-                                        <?php
-                                        $aluno_profissional = (($linha['tipo'] == 'profissional') ? $linha['id'] : false);
-                                        include 'dicas.php';
-                                        ?>
-                                        <!-- /.post -->
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-               
-    <?php } ?>
+<?php }    ?>
 
 
 
